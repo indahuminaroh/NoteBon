775c63d1d18085a7b6b7ac2f0f2b08e66af406ab
angga-nur <ancp46@gmail.com>
2017-11-22 19:11:18 +0700

    Memperbarui Edit.php

--- edit.php
+++ edit.php
@@ -1,33 +1,33 @@
-<?php
-
-include "koneksi.php";
+, <?php
+include 'koneksi.php';
 
 $id_orang = $_GET['id'];
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $nama = $_POST['nama'];
     $alamat = $_POST['alamat'];
-    $tgl_hutang = $_POST['tgl_hutang'];
-    $id_beras = $_POST['id_beras'];
-    $id_minyak = $_POST['id_minyak'];
-    $id_status = $_POST['id_status'];
-    $no_telp = $_POST['no_telp'];
+    $beras = $_POST['id_beras'];
+    $minyak = $_POST['id_minyak'];
+    $harga = $_POST['total_harga'];
+    $status = $_POST['id_status'];
+    $telp = $_POST['no_telp'];
 
-    $sql = "UPDATE orang SET nama='$nama', alamat='$alamat', tgl_hutang='$tgl_hutang', id_beras='$id_beras', id_status='$id_status', no_telp='$no_telp' WHERE id_orang='$id_orang'";
-
-    $hasil = mysqli_query($koneksi, $sql);
-
-    if($hasil){
-    echo "Berhasil Diupdate";
-    }else{
-    echo "Gagal Diupdate";
-    }
+$sql = "UPDATE orang SET nama='$nama', alamat='$alamat', id_beras='$beras', id_minyak='$minyak', total_harga='$harga', id_status='$status', no_telp='$telp' WHERE id_orang='$id_orang'";
+$query = mysqli_query($koneksi, $sql);
+header("location: index.php");
 }
 
-$sql = "SELECT nama, alamat, tgl_hutang, id_beras, id_status, no_telp FROM orang WHERE id_orang='$id_orang'";
+$sql = "SELECT nama, alamat, tgl_hutang, id_beras, id_minyak, total_harga, id_status, no_telp FROM orang WHERE id_orang='$id_orang'";
+$query = mysqli_query($koneksi, $sql);
+$hasil = mysqli_fetch_assoc($query);
 
-$data = mysqli_query($koneksi, $sql);
-$hasil = mysqli_fetch_assoc($data);
+/*var_dump($query);
+    if ($query) {
+        echo "Berhasil";
+    }else{
+        echo "gagal";
+        echo mysqli_error($koneksi);
+    }*/
 ?>
 
 <html>
@@ -37,17 +37,14 @@ $hasil = mysqli_fetch_assoc($data);
 <body>
 
 <form method="POST">
-    <label>Nama :</label><b>
+    <label>Nama :</label>
     <input type="text" name="nama" value="<?php echo $hasil['nama'] ?>"><br><br>
 
     <label>Alamat :</label>
-    <textarea name="alamat" value="<?php echo $hasil['alamat'] ?>"></textarea><br><br>
+    <input type="text" name="alamat" value="<?php echo $hasil['alamat'] ?>"><br><br>
 
-    <label>Tanggal Hutang  :</label>
-    <input type="date("Y-m-d")" name="tgl_hutang" value="<?php echo $hasil['tgl_hutang'].date("h:i:s") ?>"><br><br>
-
-    <label>Jenis Beras</label>
-    <select name="id_beras" value="<?php echo $hasil['id_orang'] ?>">
+    <label>Jenis Beras :</label>
+    <select name="id_beras" value="<?php echo $hasil['id_beras'] ?>">
         <option value="1" >Murni</option>
         <option value="2">Sembako</option>
         <option value="3">Wangi</option>
@@ -55,7 +52,7 @@ $hasil = mysqli_fetch_assoc($data);
         <option value="5">-</option>
     </select><br><br>
 
-    <label>Jenis Minyak</label>
+    <label>Jenis Minyak :</label>
     <select name="id_minyak" value="<?php echo $hasil['id_minyak'] ?>">
         <option value="1">Bimoli</option>
         <option value="2">Hemat</option>
@@ -63,10 +60,13 @@ $hasil = mysqli_fetch_assoc($data);
         <option value="4">-<option>
     </select><br><br>
 
+    <label>Total Hutang  :</label>
+    <input name="total_harga" value="<?php echo $hasil['total_harga'] ?>"><br><br>
+
     <label>Status :</label>
     <select name="id_status" value="<?php echo $hasil['id_status'] ?>">
-        <option value="Lunas">  Lunas</option>
-        <option value="Belum Lunas">Belum Lunas</option>
+        <option value="1">Lunas</option>
+        <option value="2">Belum Lunas</option>
     </select><br><br>
     <label>No Telp :<label>
         <input type="text" name="no_telp" value="<?php echo $hasil['no_telp'] ?>"><br>
@@ -75,4 +75,3 @@ $hasil = mysqli_fetch_assoc($data);
 
 </body>
 </html>
-
