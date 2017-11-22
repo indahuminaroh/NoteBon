cebe89be5586f776817e874e5f60b702f3b92602
angga-nur <ancp46@gmail.com>
2017-11-22 21:51:48 +0700

    Memperbarui edit.php : merubahnya menjadi ada cssnya

--- edit.php
+++ edit.php
@@ -1,4 +1,4 @@
-, <?php
+<?php
 include 'koneksi.php';
 
 $id_orang = $_GET['id'];
@@ -33,45 +33,70 @@ $hasil = mysqli_fetch_assoc($query);
 <html>
 <head>
 <title>Edit Hutang</title>
+<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="dist/style.css">
 </head>
 <body>
-
+<section>
+    <h2><u>Edit Data</u></h2>
+    <div class="col-sm-2">
 <form method="POST">
-    <label>Nama :</label>
-    <input type="text" name="nama" value="<?php echo $hasil['nama'] ?>"><br><br>
-
-    <label>Alamat :</label>
-    <input type="text" name="alamat" value="<?php echo $hasil['alamat'] ?>"><br><br>
-
-    <label>Jenis Beras :</label>
-    <select name="id_beras" value="<?php echo $hasil['id_beras'] ?>">
-        <option value="1" >Murni</option>
-        <option value="2">Sembako</option>
-        <option value="3">Wangi</option>
-        <option value="4">Merah</option>
-        <option value="5">-</option>
-    </select><br><br>
-
-    <label>Jenis Minyak :</label>
-    <select name="id_minyak" value="<?php echo $hasil['id_minyak'] ?>">
-        <option value="1">Bimoli</option>
-        <option value="2">Hemat</option>
-        <option value="3">Fortune</option>
-        <option value="4">-<option>
-    </select><br><br>
-
-    <label>Total Hutang  :</label>
-    <input name="total_harga" value="<?php echo $hasil['total_harga'] ?>"><br><br>
-
-    <label>Status :</label>
-    <select name="id_status" value="<?php echo $hasil['id_status'] ?>">
-        <option value="1">Lunas</option>
-        <option value="2">Belum Lunas</option>
-    </select><br><br>
-    <label>No Telp :<label>
-        <input type="text" name="no_telp" value="<?php echo $hasil['no_telp'] ?>"><br>
-
-    <input style="margin-left: 300px; margin-top: 30px;" type="submit" name="submit" value="Simpan">    
-
+    <table class="table table-stripped">
+        <tr>
+            <td><label>Nama :</label></td>
+            <td><input type="text" name="nama" value="<?php echo $hasil['nama'] ?>" required/></td>
+        </tr>
+        <tr>
+            <td><label>Alamat :</label></td>
+            <td><input type="text" name="alamat" value="<?php echo $hasil['alamat'] ?>" required/></td>
+        </tr>
+        <tr>
+            <td><label>Jenis Beras :</label></td>
+            <td><select name="id_beras" value="<?php echo $hasil['id_beras'] ?>">
+                    <option value="1" >Murni</option>
+                    <option value="2">Sembako</option>
+                    <option value="3">Wangi</option>
+                    <option value="4">Merah</option>
+                    <option value="5">-</option>
+            </select></td>
+        </tr>
+        <tr>
+            <td><label>Jenis Minyak :</label></td>
+            <td><select name="id_minyak" value="<?php echo $hasil['id_minyak'] ?>">
+                    <option value="1">Bimoli</option>
+                    <option value="2">Hemat</option>
+                    <option value="3">Fortune</option>
+                    <option value="4">-</option>
+            </select></td>
+        </tr>
+        <tr>
+            <td><label>Total Hutang  :</label></td>
+            <td><input name="total_harga" value="<?php echo $hasil['total_harga'] ?>" required/></td>
+        </tr>
+        <tr>
+            <td><label>Status :</label></td>
+            <td><select name="id_status" value="<?php echo $hasil['id_status'] ?>">
+                    <option value="1">Lunas</option>
+                    <option value="2">Belum Lunas</option>
+                </select></td>
+        </tr>
+            <td><label>No Telp :</label></td>
+            <td><input type="text" name="no_telp" value="<?php echo $hasil['no_telp'] ?>" required/></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input class="btn btn-primary" style="margin-left: 300px; margin-top: 30px;" type="submit" name="submit" value="Simpan"></td>
+        </tr>    
+</table>
+</form>
+</div>
+</section>
+<aside>
+    <h3><u>Ketentuan dan Syarat Hutang</u></h3>
+            
+            <p>1. Nama Pelanggan harus jelas.</p>
+            <p>2. Harus meninggalkan alamat rumah dan no_telp.</p>
+            <p>3. Harus membayar hutang.</p>
+</aside>
 </body>
 </html>
